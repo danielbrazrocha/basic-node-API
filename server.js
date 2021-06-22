c736bc57218517bb2b270955341a46c23f2dc988
Daniel Rocha <dgustavo@id.uff.br>
2021-06-22 14:44:47 -0300

    add planos, tarifas and gitignore

--- server.js
+++ server.js
@@ -2,18 +2,39 @@ var express = require('express'),
 app = express(),
 port = process.env.PORT || 3000;
 
+tarifas = {
+    11: {
+      16: 1.9,
+      17: 1.7,
+      18: 0.9,
+    },
+    16: {
+      11: 2.9,
+    },
+    17: {
+      11: 2.7,
+    },
+    18: {
+      11: 1.9,
+    },
+};
 
-obj = {
+objDDD = {
     origem: 
         ['11', '16', '17', '18'],
     destino:
         ['11', '16', '17', '18']
-}
+};
+
+planos = ['Mais 30', 'Mais 60', 'Mais 120'];
+
+
 
 app.listen(port);
 
-app.get("/getDDD", (req, res, next) => { res.json(obj); });
+app.get("/ddd", (req, res, next) => { res.json(objDDD); });
+app.get("/planos", (req, res, next) => { res.json(planos); });
+app.get("/tarifas", (req, res, next) => { res.json(tarifas); });
 
-app.get("/url", (req, res, next) => { res.json(["Tony","Lisa","Michael","Ginger","Food"]); });
 
 console.log('todo list RESTful API server started on: ' + port);
